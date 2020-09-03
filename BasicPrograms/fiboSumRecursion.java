99ce2e759736bf2519d65ffc105ce28a6c5cbeab
Shouvik <49680243+shouvik78@users.noreply.github.com>
2020-09-03 12:37:08 +0530

    Add files via upload

---
+++ BasicPrograms/fiboSumRecursion.java
@@ -0,0 +1,41 @@
+//sum of fibo series
+/*
+package BasicPrograms;
+
+public class fiboSumRecursion {
+	static int fib(int n)
+	{
+		if(n<=1) return n;
+		return fib(n-1)+fib(n-2);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=50;
+		System.out.println(fib(n));
+	}
+
+}*/
+package BasicPrograms;
+
+public class fiboSumRecursion {
+	private int x=0;
+	public static void main(String args[])
+	{
+		new fiboSumRecursion().test();
+	}
+	private int f(int x)
+	{
+		return ++x;
+	}
+	private int g(int y)
+	{
+		return x++;
+	
+	}
+	private void test()
+	{
+		System.out.print(f(x)==f(x)? "f":"#");
+		System.out.print (g(x)==g(x)? "g":"#");
+	}
+}
---
+++ BasicPrograms/primeNum1toN.java
@@ -0,0 +1,28 @@
+package BasicPrograms;
+import java.util.*;
+
+public class primeNum1toN {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("please enter the number upto which you want prime number");
+		int n=sc.nextInt();
+		int i,j;
+		for(i=2;i<=n;i++)
+		{
+			int c=0;
+			for(j=1;j<=i;j++)
+			{
+				if(i%j==0)
+				{
+					c++;
+				}
+			}
+			if(c==2)
+				System.out.print(+i+" ");
+		}
+		
+	}
+
+}
